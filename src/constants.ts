9c478f175773c06f7ea603495cf27b0e72c4fd24
Guesslot <guesslot@outlook.com>
2023-07-14 00:02:22 +0800

    update

--- src/constants.ts
+++ src/constants.ts
@@ -1,18 +1,6 @@
 export const Settings: any = {
   DefaultChain: 137,
   Chains: {
-    137: {
-      chainId: '0x89',
-      chainName: 'Polygon',
-      rpcUrls: ['https://polygon-rpc.com/'],
-      nativeCurrency: {
-        name: 'MATIC',
-        symbol: 'MATIC',
-        decimals: 18,
-      },
-      blockExplorerUrls: ['https://polygonscan.com/'],
-      infuraId: '',
-    },
     80001: {
       chainId: '0x13881',
       chainName: 'Mumbai',
@@ -25,20 +13,28 @@ export const Settings: any = {
       blockExplorerUrls: ['https://mumbai.polygonscan.com/'],
       infuraId: '',
     },
-    1: {
-      chainId: '0x1',
-      chainName: 'Ethereum',
+    137: {
+      chainId: '0x89',
+      chainName: 'Polygon',
+      rpcUrls: ['https://polygon-rpc.com/'],
+      nativeCurrency: {
+        name: 'MATIC',
+        symbol: 'MATIC',
+        decimals: 18,
+      },
+      blockExplorerUrls: ['https://polygonscan.com/'],
+      infuraId: '',
     },
   },
   Tokens: {
-    137: {},
     80001: {},
+    137: {},
   },
   Wallets: {
     WalletConnect: {
       rpc: {
-        137: 'https://polygon-rpc.com/',
         80001: 'https://matic-mumbai.chainstacklabs.com',
+        137: 'https://polygon-rpc.com/',
       },
     },
   },
--- src/subgraph.ts
+++ src/subgraph.ts
@@ -1,15 +1,16 @@
 import axios from 'axios';
-import { AddressZero } from '@ethersproject/constants';
-import { formatEther } from '@ethersproject/units';
+// import { AddressZero } from '@ethersproject/constants';
+// import { formatEther } from '@ethersproject/units';
 
 export default class Subgraph {
-  private api: string = 'https://api.thegraph.com/subgraphs/name/guesslot/v2';
+  private thegraphApi: string = 'https://api.thegraph.com/subgraphs/name/guesslot/v2';
+  private githubApi: string = 'https://raw.githubusercontent.com/guesslot/guesslot-events/main/events.json';
+  private events: any;
 
   private async request(query: string, data: any): Promise<any> {
-    if (!this.api) throw new Error('Subgraph Config Error');
     return axios
       .post(
-        this.api,
+        this.thegraphApi,
         JSON.stringify({
           query: query,
           variables: data,
@@ -19,5 +20,25 @@ export default class Subgraph {
         if (resp.data.data) return resp.data.data.data;
         return Promise.reject(resp.data.errors);
       });
+  }
+
+  private async initEvents(): Promise<any> {
+    if (this.events) return;
+    const resp: any = await axios.get(this.githubApi);
+    this.events = resp.data;
+  }
+
+  public async getEvents(skip: number = 0): Promise<any> {
+    await this.initEvents();
+    const query: string =
+      'query ($skip: Int!) {data:events(first: 50, skip: $skip, orderBy: startTime, orderDirection: desc, where: {}) {token, pool, name, epoch, startTime, endTime, settleTime, count, stakes, status}}';
+    return this.request(query, { skip: skip }).then((data: any) => {
+      const items: any = [];
+      data.forEach((item: any) => {
+        const evt: any = this.events[item.pool];
+        if (evt) items.push(Object.assign(item, evt[item.name]));
+      });
+      return items;
+    });
   }
 }
